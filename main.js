8fb3c9d4f630683d091e993ed8a8c1939cc573ad
Anfimov Vladimir <vladimir@safehouse.ro>
2023-04-04 19:08:13 +0300

    layout step 1 done

---
+++ main.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const burgerButton = document.querySelector("#burger-icon");
+const closeButton = document.querySelector(".sidebar-close");
+
+burgerButton.addEventListener("click", toggleMenu);
+closeButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  console.log("clicked");
+  const navMenu = document.querySelector("#sidebar-element");
+  navMenu.classList.toggle("sidebar-active");
+}
